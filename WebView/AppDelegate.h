cfa9ab82357c9d96f46fc372c96db6579e80b9d9
Li Yong <btt365com@gmail.com>
2018-08-01 16:14:55 +0800

    Initial Commit

---
+++ WebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebView
+//
+//  Created by bet001 on 2018/8/1.
+//  Copyright © 2018年 bet365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebView
+//
+//  Created by bet001 on 2018/8/1.
+//  Copyright © 2018年 bet365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
